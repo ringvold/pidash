d11602d4a6ee7d71f468e93ab0f17aab57218a1f
Harald Ringvold <harald.ringvold@gmail.com>
2018-02-02 23:33:25 +0100

    Fix build script for new package struckture

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 (cd frontend && yarn run build)
 statik --src=frontend/dist
-gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="linux/386 linux/amd64 linux/arm linux/arm64 darwin/amd64"
+gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="linux/386 linux/amd64 linux/arm linux/arm64 darwin/amd64" ./cmd/*
