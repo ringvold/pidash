ca3fbcd7e0735b739f61ab4cb86cf89189456b70
Harald Ringvold <harald.ringvold@gmail.com>
2018-02-03 00:12:49 +0100

    Slightly more fancy build script

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+echo "Start building pi-dash"
+echo ""
+echo "Building frontend"
 (cd frontend && yarn run build)
+echo ""
+echo "Embeding frontend into Go app"
 statik --src=frontend/dist
-gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="linux/386 linux/amd64 linux/arm linux/arm64 darwin/amd64" ./cmd/*
+echo ""
+echo "Building go binaries to ./build"
+gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}" \
+    -osarch="linux/386 linux/amd64 linux/arm linux/arm64 darwin/amd64" \
+    ./cmd/*
+echo ""
+echo "ALL DONE! :D"
